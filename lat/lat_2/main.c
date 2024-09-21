8097f16904765b8fbc725a9b1aa529d4a7314fab
RadyaI <radyaiftikhar@gmail.com>
2024-09-21 09:19:24 +0700

    coba input user kek mana

--- lat/lat_2/main.c
+++ lat/lat_2/main.c
@@ -1,5 +1,16 @@
-#include <stdio.h>
+    #include <stdio.h>
 
-void main(){
-    printf("Belum ada...");
-}
+    int main(){
+        int umur;
+        char nama[50];
+
+        printf("Masukkan nama: ");
+        scanf("%49s", nama);
+        printf("Masukkan umur: ");
+        scanf("%d", &umur);
+
+        printf("\nNama anda adalah: %s \n", nama);
+        printf("Umur anda: %d", umur);
+
+        return 0;
+    }
