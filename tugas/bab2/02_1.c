23809650874c44d5f00cffca92d1dad92c32ec3b
RadyaI <radyaiftikhar@gmail.com>
2024-10-05 14:02:30 +0700

    lengkapin

--- tugas/bab2/02_1.c
+++ tugas/bab2/02_1.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    int jumlah;
-    char jawaban;
-    double suhu_awal;
-    float radius;
-    double harga;
-    int nilai_hexa;
-    int nilai_oktal;
+    int jumlah = 0;
+    char jawaban = 'B';
+    double suhu_awal = 25.0;
+    float radius = 0.0;
+    double harga = 10000.0;
+    int nilai_hexa = 0x1A;
+    int nilai_oktal = 022;
 
     return 0;
 }
--- tugas/bab2/02_2.c
+++ tugas/bab2/02_2.c
@@ -2,7 +2,8 @@
 
 int main(){
     const float PHI = 3.14;
-    
-    
-
+    const int MAXSIZE = 255;
+    const char ROOTDIR = 'C';
+    const float MIN_KELVIN = 80.0;
+    const float R = 8.314472;
 }
