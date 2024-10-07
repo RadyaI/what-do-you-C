6f8cb0d36372d87a7c6468ba179f297e83b97afc
RadyaI <radyaiftikhar@gmail.com>
2024-10-07 21:47:50 +0700

    naise

---
+++ tugas/bab5/soal10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char input[3];
+
+    printf("Masukkan 2 karakter: ");
+    scanf("%2s", input);
+
+    if(strcmp(input, "ab") == 0 || strcmp(input, "ba") == 0){
+        printf("Silahkan Masuk!");
+    } else {
+        printf("Identifikasi Gagal");
+    }
+
+    return 0;
+}
