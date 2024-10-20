a39895b2c4a94b145bd554e0964e45bcb08e404d
RadyaI <radyaiftikhar@gmail.com>
2024-10-20 18:54:00 +0700

    codelab 1 keknya ga ada yg salah ntahlah

--- praktek/modul3/codelab1.c
+++ praktek/modul3/codelab1.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
-int main()
-{
+
+int main() {
     int umur;
+
     printf("Masukkan umur penumpang: ");
     scanf("%d", &umur);
-    if (umur >= 0 && umur <= 120)
-    {
-        if (umur < 2)
-        {
+
+    if (umur >= 0 && umur <= 120) {
+        if (umur < 2) {
             printf("Penumpang mendapatkan tiket gratis.\n");
-        }
-        else if (umur >= 2 && umur <= 12)
-        {
+        } else if (umur >= 2 && umur <= 12) {
             printf("Harga tiket untuk penumpang anak-anak adalah 50%% dari harga normal.\n");
-        }
-        else if (umur > 12 && umur <= 60)
-        {
+        } else if (umur > 12 && umur <= 60) {
             printf("Harga tiket untuk penumpang dewasa adalah harga normal.\n");
-        }
-        else
-        {
+        } else {
             printf("Harga tiket untuk penumpang lanjut usia adalah 75%% dari harga normal.\n");
         }
-    }
-    else
-    {
+    } else {
         printf("Input tidak valid. Umur harus berada dalam rentang 0 hingga 120.\n");
     }
+
     return 0;
 }
