570cbc652ab6f476008f4df4841af96662dff89e
RadyaI <radyaiftikhar@gmail.com>
2024-12-03 18:26:36 +0700

    codelab 2 modul 5

--- praktek/modul5/codelab2.c
+++ praktek/modul5/codelab2.c
@@ -1,1 +1,17 @@
-//otw
+#include <stdio.h>
+
+int main()
+{
+
+    char produk[][50] = {"Sikat Gigi", "Odol", "Kipas"};
+    int toko1[] = {5000, 8000, 6000};
+    int toko2[] = {3000, 7000, 9000};
+
+    printf("Total penjualan untuk setiap produk: \n");
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%s: %d\n", produk[i], toko1[i] + toko2[i]);
+    }
+
+    return 0;
+}
