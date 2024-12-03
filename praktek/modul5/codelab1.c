ede0bb0786ef7bc065df65c0317543fd5b1f3fb0
RadyaI <radyaiftikhar@gmail.com>
2024-12-03 18:11:49 +0700

    codelab 1 modul 5

--- praktek/modul5/codelab1.c
+++ praktek/modul5/codelab1.c
@@ -1,1 +1,28 @@
-//otw
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    char nama[][50] = {"Alice", "Bob", "Charlie", "David", "Eva"};
+    int nilai[] = {85, 90, 78, 92, 88};
+
+    printf("Daftar nama dan nilai siswa sebelum diubah: \n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s: %d\n", nama[i], nilai[i]);
+    }
+
+    printf("\nDaftar nama dan nilai siswa setelah diubah: \n");
+    for (int i = 0; i < 5; i++)
+    {
+        if (strcmp(nama[i], "David") == 0)
+        {
+            nilai[i] = 95;
+        }
+
+        printf("%s: %d\n", nama[i], nilai[i]);
+    }
+
+    return 0;
+}
