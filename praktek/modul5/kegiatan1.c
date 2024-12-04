c086cdae57b4127ee481f6fce4ba3b43ef84c9fd
RadyaI <radyaiftikhar@gmail.com>
2024-12-04 13:48:00 +0700

    oke kegiatan 1 modul 5 dah fix

--- praktek/modul5/kegiatan1.c
+++ praktek/modul5/kegiatan1.c
@@ -2,67 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-int bandingkan(const void *a, const void *b);
-int mintaJumlahBuah();
-void inputNamaBuah(char buah[][50], int n);
-void tampilkanUrutanInput(char buah[][50], int n);
-void tampilkanUrutanAbjad(char buah[][50], int n);
-void urutkanBuah(char buah[][50], int n);
+int bandingkanBuah(const void *a, const void *b);
+void kelolaBuah();
 
 int main() {
-    int n;
-    char buah[10][50];
-
-    n = mintaJumlahBuah();
-    if (n == -1) {
-        return 1; 
-    }
-    inputNamaBuah(buah, n);
-    tampilkanUrutanInput(buah, n);
-    urutkanBuah(buah, n);
-    tampilkanUrutanAbjad(buah, n);
-
+    kelolaBuah();
     return 0;
 }
 
-int bandingkan(const void *a, const void *b) {
-    return strcmp((char *)a, (char *)b);
-}
-
-int mintaJumlahBuah() {
+void kelolaBuah() {
     int n;
+    char buah[10][50];
+
     printf("Masukkan jumlah buah (maksimal 10): ");
     scanf("%d", &n);
-
     if (n > 10 || n <= 0) {
         printf("Jumlah buah harus antara 1 sampai 10.\n");
-        return -1; 
+        return;
     }
 
-    return n;
-}
-
-void inputNamaBuah(char buah[][50], int n) {
     for (int i = 0; i < n; i++) {
         printf("Masukkan nama buah ke-%d: ", i + 1);
         scanf("%s", buah[i]);
     }
-}
 
-void tampilkanUrutanInput(char buah[][50], int n) {
     printf("\nNama buah dalam urutan yang diinputkan:\n");
     for (int i = 0; i < n; i++) {
         printf("%s\n", buah[i]);
     }
-}
 
-void tampilkanUrutanAbjad(char buah[][50], int n) {
+    qsort(buah, n, sizeof(buah[0]), bandingkanBuah);
+
     printf("\nNama buah dalam urutan abjad:\n");
     for (int i = 0; i < n; i++) {
         printf("%s\n", buah[i]);
     }
 }
 
-void urutkanBuah(char buah[][50], int n) {
-    qsort(buah, n, sizeof(buah[0]), bandingkan);
+int bandingkanBuah(const void *a, const void *b) {
+    return strcmp((const char *)a, (const char *)b);
 }
