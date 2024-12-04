bd70459aa03c8bf1d9007f87215c21283ea4bdfb
RadyaI <radyaiftikhar@gmail.com>
2024-12-04 09:08:43 +0700

    praktek 04/12/24

--- praktek/modul5/tes.c
+++ praktek/modul5/tes.c
@@ -1,35 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-    // Deklarasi array 2D untuk nama dan nilai
-    char names[5][20] = {"Alice", "Bob", "Charlie", "David", "Eva"};
-    int scores[5] = {85, 90, 78, 92, 88};
-    int i, j;
-
-    // Menampilkan daftar siswa dan nilai sebelum diubah
-    printf("Daftar siswa dan nilai sebelum diubah:\n");
-    for (i = 0; i < 5; i++)
-    {
-        printf("%s: %d\n", names[i], scores[i]);
-    }
-
-    // perulangan disini tak apus
-
-    // Menampilkan daftar siswa dan nilai setelah diubah
-    printf("\nDaftar siswa dan nilai setelah diubah:\n");
-    for (j = 0; j < 5; j++)
-    {
-        // buat cek nilai/ubah pindah kesini
-        if (strcmp(names[j], "David") == 0)
-        {
-            scores[j] = 95;
-            break;
-        }
-
-        printf("%s: %d\n", names[j], scores[j]);
-    }
-
+    char nama[3][50] = {"Muhammad", "Radya", "Iftikhar"};
+    printf("%c", nama[0][7]);
     return 0;
 }
